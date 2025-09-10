148ee11e4d6bb9cd3fbc48240708b056ba67d4bd
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-09-10 10:59:11 -0300

    publish(771623367968): 2 script.js

---
+++ 771623367968/aval-2/script.js
@@ -0,0 +1,7 @@
+echo "# avalia-o1" >> README.md 
+git init 
+git add README.md 
+git commit -m "primeiro commit" 
+git branch -M main 
+git remote add origin https://github.com/isaacnewton00/avalia-o1.git
+ git push -u origin main
