cd8b4b748062b978ce1187308a7e19f679918bb5
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-09-25 13:24:57 -0300

    publish(35465256010): 2

---
+++ 35465256010/aval-2/script.js
@@ -0,0 +1,6 @@
+function iniciarJogo() {
+    const mensagem = document.getElementById("mensagem");
+    mensagem.textContent = "🎮 O jogo começou! Capture o Jerry antes que ele fuja!";
+    
+    // Aqui pode-se iniciar a lógica do jogo real futuramente
+}
