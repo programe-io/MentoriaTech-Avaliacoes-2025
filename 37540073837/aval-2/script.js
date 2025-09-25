00555f8364dc1b962f0b1462866734fd56d3354a
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-09-25 12:02:15 -0300

    publish(37540073837): 2

---
+++ 37540073837/aval-2/script.js
@@ -0,0 +1,16 @@
+.menu-lateral button {
+    margin-top: 20px;
+    padding: 10px;
+    width: 100%;
+    border: none;
+    background-color: #d81b60;
+    color: white;
+    font-weight: bold;
+    border-radius: 5px;
+    cursor: pointer;
+    transition: background 0.3s ease;
+}
+
+.menu-lateral button:hover {
+    background-color: #ad1457;
+}
