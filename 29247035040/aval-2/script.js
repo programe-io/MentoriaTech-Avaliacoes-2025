a67091acb1d3c83ec07fe7d95e3816b3dd1bef55
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-09-26 19:41:46 -0300

    publish(29247035040): 2

---
+++ 29247035040/aval-2/script.js
@@ -0,0 +1,9 @@
+// script.js - Interações da Página do Eziel
+
+// Mostra uma mensagem quando o botão é clicado
+function mostrarMensagem() {
+    alert("Valeu por visitar a página do Eziel! ⚽🎮👨‍👩‍👧‍👦");
+}
+
+// Exemplo extra: mostra uma mensagem no console
+console.log("Página carregada com sucesso!");
