ea232785ad91a4139aff8a452cfe71fa1ddf2ad3
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-09-26 21:31:37 -0300

    publish(29252155708): 2

---
+++ 29252155708/aval-2/script.js
@@ -0,0 +1,9 @@
+// Arquivo script.js
+
+// Este código executa quando a página é totalmente carregada.
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Página "Sobre Mim" de Glenda Tavares carregada e pronta!');
+
+    // Se você quiser adicionar alguma funcionalidade interativa (como um menu
+    // que abre/fecha ou uma galeria de imagens), o código virá aqui.
+});
