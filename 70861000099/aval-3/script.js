29fe4009dd7ff30b78cc46afb496c848a0738e4e
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-09-28 14:04:19 -0300

    publish(70861000099): 3

---
+++ 70861000099/aval-3/script.js
@@ -0,0 +1,19 @@
+/* Reset básico */
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+/* Corpo da página */
+body {
+  font-family: 'Roboto', sans-serif;
+  background-color: #fff;
+  color: #333;
+  line-height: 1.6;
+  padding: 0 1rem;
+}
+
+/* Cabeçalho */
+.main-header {
+  text-align: center;
