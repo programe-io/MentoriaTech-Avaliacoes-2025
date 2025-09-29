88a739982a40339e65561fc251a55682922172fa
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-09-29 11:45:11 -0300

    publish(58948612204): 2

---
+++ 58948612204/aval-2/script.js
@@ -0,0 +1,7 @@
+// Menu hambúrguer para celular
+const menuToggle = document.querySelector(".menu-toggle");
+const menu = document.querySelector(".menu");
+
+menuToggle.addEventListener("click", () => {
+  menu.classList.toggle("show");
+});
