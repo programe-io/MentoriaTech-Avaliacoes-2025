3f3e7437782b7a57f184eecf4e1224006785b865
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-10-01 14:20:27 -0300

    publish(71515675815): 2

---
+++ 71515675815/aval-2/script.js
@@ -0,0 +1,6 @@
+const menuBtn = document.querySelector('.menu-btn');
+const navbar = document.getElementById('navbar');
+
+menuBtn.addEventListener('click', () => {
+  navbar.classList.toggle('show');
+});
