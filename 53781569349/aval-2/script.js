aa07aadaf3713d3aefbd946b5c5c24c971e57928
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-10-01 15:37:49 -0300

    publish(53781569349): 2

---
+++ 53781569349/aval-2/script.js
@@ -0,0 +1,4 @@
+// Alternar a visibilidade do menu no mobile
+document.querySelector('.menu-btn').addEventListener('click', function () {
+  document.getElementById('navbar').classList.toggle('show');
+});
