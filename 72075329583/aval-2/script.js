4f684deaf9a4d528ab270291d8641f9712ceb024
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-10-01 14:01:45 -0300

    publish(72075329583): 2

---
+++ 72075329583/aval-2/script.js
@@ -0,0 +1,5 @@
+function jogar(nomeJogo) {
+  alert("Você escolheu jogar: " + nomeJogo);
+  // Aqui você pode redirecionar para uma página do jogo, por exemplo:
+  // window.location.href = 'jogos/' + nomeJogo.toLowerCase() + '.html';
+}
