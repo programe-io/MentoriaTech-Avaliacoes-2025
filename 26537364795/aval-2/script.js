d01af4124107ae7566fcd1b696d5dde7dd159ea6
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-10-02 10:21:57 -0300

    publish(26537364795): 2

---
+++ 26537364795/aval-2/script.js
@@ -0,0 +1,5 @@
+<div class="galeria">
+    <img src="https://super.abril.com.br/wp-content/uploads/2021/04/Marie-Curie-a-polonesa-mais-brilhante-da-historia.png?crop=1&resize=1212,909"
+      <img src="https://s2.static.brasilescola.uol.com.br/be/2020/11/marie-curie-marido.jpg" alt="Retrato de Marie Curie">
+      <img src="https://static.todamateria.com.br/upload/ma/ri/mariecurie-cke.jpg" alt="Marie Curie no laboratório">
+    </div>
