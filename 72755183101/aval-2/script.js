f486a6bf30a27e709a31625aa086e0ce115eec57
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-10-02 11:39:02 -0300

    publish(72755183101): 2

---
+++ 72755183101/aval-2/script.js
@@ -0,0 +1,7 @@
+const form = document.getElementById('formContato');
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Mensagem enviada com sucesso! 💌');
+    form.reset();
+});
