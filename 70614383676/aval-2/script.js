4f1e86eb24c0835f7ce55a1e38de687e9ac04d2d
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-10-03 08:37:57 -0300

    publish(70614383676): 2

---
+++ 70614383676/aval-2/script.js
@@ -0,0 +1,7 @@
+function tocarMusica() {
+  alert("🎵 Tocando: O Pintinho Amarelinho 🎵");
+
+  // Se você tiver um arquivo de áudio, pode fazer assim:
+  // const audio = new Audio('pintinho.mp3');
+  // audio.play();
+}
