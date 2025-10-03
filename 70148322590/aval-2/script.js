d202fde376ffca0943424d1e2f516969f2196eee
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-10-03 07:54:47 -0300

    publish(70148322590): 2

---
+++ 70148322590/aval-2/script.js
@@ -0,0 +1,4 @@
+function mostrarMensagem() {
+  const msg = document.getElementById("mensagem");
+  msg.innerText = "Messi também é o jogador com mais bolas de ouro da história, com 8 conquistas!";
+}
