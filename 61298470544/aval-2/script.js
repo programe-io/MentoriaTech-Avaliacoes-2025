46119cf25886cfc74188450a53da23aff81a38f9
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-10-07 15:29:36 -0300

    publish(61298470544): 2

---
+++ 61298470544/aval-2/script.js
@@ -0,0 +1,4 @@
+// Função para abrir e fechar o menu no celular
+document.querySelector('.menu-btn').addEventListener('click', () => {
+  document.getElementById('navbar').classList.toggle('active');
+});
