db98fda640cb75a2fc31f54cb1ad3a965e180492
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-10-14 15:10:29 -0300

    publish(75307656084): 2

---
+++ 75307656084/aval-2/script.js
@@ -0,0 +1,4 @@
+// Exemplo simples de interação com o botão
+document.getElementById("botao").addEventListener("click", function() {
+    alert("Olá, Maria Luiza! 👋 Obrigado por visitar meu site!");
+});
