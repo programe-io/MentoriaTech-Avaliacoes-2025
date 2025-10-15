2b0ae966e159299bc674131e32fe07618da95bc9
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-10-15 17:36:43 -0300

    publish(65113977551): 2

---
+++ 65113977551/aval-2/script.js
@@ -0,0 +1,3 @@
+document.getElementById("btnYoutube").addEventListener("click", function() {
+      window.open("https://m.youtube.com/watch?v=XX3dmVrSRA4&pp=ygUMYXlydG9uIHNlbm5h", "_blank");
+      });
