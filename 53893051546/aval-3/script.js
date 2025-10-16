92636ca616325fbe782919ec0939fb00c4b1749b
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-10-16 13:20:57 -0300

    publish(53893051546): 3

--- 53893051546/aval-3/script.js
+++ 53893051546/aval-3/script.js
@@ -1,14 +1,1 @@
-// Script simples para navegação suave ao clicar nos links do menu
-document.querySelectorAll('nav a').forEach(link => {
-  link.addEventListener('click', e => {
-    e.preventDefault();
-    const targetId = link.getAttribute('href').substring(1);
-    const targetSection = document.getElementById(targetId);
-    if (targetSection) {
-      window.scrollTo({
-        top: targetSection.offsetTop - 60,
-        behavior: 'smooth',
-      });
-    }
-  });
-});
+
