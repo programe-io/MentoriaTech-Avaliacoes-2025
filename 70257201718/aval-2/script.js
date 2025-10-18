03e39b662c5b9b509ae8836dbf0d28157a574c67
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-10-17 23:23:53 -0300

    publish(70257201718): 2

---
+++ 70257201718/aval-2/script.js
@@ -0,0 +1,5 @@
+// Função para mostrar uma mensagem de boas-vindas
+function mostrarMensagem() {
+    const mensagemDiv = document.getElementById('mensagem');
+    mensagemDiv.innerHTML = '<p>Bem-vindo ao mundo das motos! Curta sua experiência!</p>';
+}
