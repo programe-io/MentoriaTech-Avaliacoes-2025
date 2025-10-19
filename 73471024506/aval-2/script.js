df8a57e5b3bd0f7ba9ffbbd919acd02ddd7e3874
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-10-19 14:54:08 -0300

    publish(73471024506): 2

---
+++ 73471024506/aval-2/script.js
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>A Távola Geek | Blog de RPG</title>
+    
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>
+            <span class="logo-icon"></span> 
+            <a href="#home" class="router-link" style="color: inherit; text-decoration: none;">A Távola Geek</a>
+            <span class="logo-icon"></span>
+        </h1>
+        <nav>
+            <a href="#home" class="router-link">Home</a> | 
+            <a href="#sistemas" class="router-link">Sistemas</a> | 
+            <a href="#mestres" class="router-link">Mestres</a> | 
+            <a href="#aventuras" class="router-link">Aventuras</a> | 
+            <a href="#sobre" class="router-link">Sobre</a>
+        </nav>
+    </header>
+
+    <div id="content-wrapper">
+        <main id="container-posts">
+            <h2>Carregando...</h2>
+        </main>
+        
+        <aside id="sidebar">
+            <h3>Recursos Rápidos</h3>
+            <ul id="aside-links">
+                </ul>
+        </aside>
+    </div>
+    
+    <footer>
+        <p>&copy; 2025 RPG & Afins. Role um D20 para o seu dia!</p>
+    </footer>
+
+    <script src="script.js"></script>
+</body>
+</html>
