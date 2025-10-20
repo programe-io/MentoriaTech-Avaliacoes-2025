167c8e5c4c7560e341aa5470c82e3fa02290f8a4
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-10-20 13:51:31 -0300

    publish(39098680500): 2

---
+++ 39098680500/aval-2/script.js
@@ -0,0 +1,4 @@
+// Mostra mensagem ao clicar em "Curtir"
+document.getElementById("curtirBtn").addEventListener("click", function() {
+    document.getElementById("mensagemCurtir").textContent = "Você curtiu esta postagem! ❤️";
+});
