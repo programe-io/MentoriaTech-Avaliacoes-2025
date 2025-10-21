b71a52f6eaa91cd2acb05d54dd6ceeafca083c9d
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-10-21 09:42:00 -0300

    publish(65680547767): 3

---
+++ 65680547767/aval-3/script.js
@@ -0,0 +1,9 @@
+
+
+let cartCount = 0;
+
+function addToCart(productName) {
+    cartCount++;
+    document.getElementById('cart-count').textContent = cartCount;
+    alert(`✔️ "${productName}" adicionado ao carrinho!`);
+}
