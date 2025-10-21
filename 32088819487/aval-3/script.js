6a126637a749216d6d6f37d4ba25e1ad315a6a1c
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-10-21 09:55:52 -0300

    publish(32088819487): 3

---
+++ 32088819487/aval-3/script.js
@@ -0,0 +1,26 @@
+<header>
+    <div class="container">
+        <h1>Chic Vestidos</h1>
+        <nav>
+            <ul>
+                <li><a href="#lancamentos">Lançamentos</a></li>
+                <li><a href="#">🛒 Carrinho (<span id="contador-carrinho">0</span>)</a></li>
+                <li><a href="#contato">Contato</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
+<div class="lista-produtos">
+    <div class="produto-card">
+        <button class="btn-comprar" onclick="adicionarAoCarrinho('Vestido Longo Elegance')">Adicionar ao Carrinho</button>
+    </div>
+
+    <div class="produto-card">
+        <button class="btn-comprar" onclick="adicionarAoCarrinho('Vestido Curto Floral')">Adicionar ao Carrinho</button>
+    </div>
+    </div>
+
+<script src="script.js"></script>
+
+</body>
+</html>
