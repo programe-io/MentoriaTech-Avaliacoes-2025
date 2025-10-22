e1b67eefe5a8db9005516f5bd7fb367e9894a075
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-10-22 13:28:00 -0300

    publish(72768206733): 2

---
+++ 72768206733/aval-2/script.js
@@ -0,0 +1,31 @@
+<section id="cardapio" class="secao-padrao">
+            <h3>Nosso Cardápio</h3>
+            
+            <h4>Cafés Especiais</h4>
+            
+            <div id="seletor-cafe">
+                <div class="opcao-cafe">
+                    <input type="radio" id="pequeno" name="tamanho" value="6.00" checked>
+                    <label for="pequeno">Pequeno (R$ 6,00)</label>
+                </div>
+                <div class="opcao-cafe">
+                    <input type="radio" id="medio" name="tamanho" value="8.50">
+                    <label for="medio">Médio (R$ 8,50)</label>
+                </div>
+                <div class="opcao-cafe">
+                    <input type="radio" id="grande" name="tamanho" value="11.00">
+                    <label for="grande">Grande (R$ 11,00)</label>
+                </div>
+
+                <p><strong>Preço Selecionado:</strong> <span id="preco-final">R$ 6,00</span></p>
+            </div>
+
+            <h4>Doces & Salgados</h4>
+            <ul>
+                <li>Bolo de Cenoura com Cobertura de Chocolate - R$ 9,00</li>
+                <li>Croissant de Amêndoas - R$ 11,00</li>
+                <li>Pão de Queijo Recheado (Porção) - R$ 8,00</li>
+            </ul>
+
+            <a href="#" class="botao-secundario">Baixar Cardápio Completo (PDF)</a>
+        </section>
