0f2a840ac201cfb5b161574c47db718b8ba315aa
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-10-24 15:57:13 -0300

    publish(70753820595): 2

---
+++ 70753820595/aval-2/script.js
@@ -0,0 +1,7 @@
+/* script.js */
+const curiosidades = [
+  'O nome Moana significa "oceano" em várias línguas polinésias.',
+    'A atriz Auli’i Cravalho tinha apenas 14 anos quando dublou Moana.',
+      'O personagem Maui é inspirado em lendas reais da Polinésia.',
+        'Cada tatuagem de Maui conta uma parte da sua história.'
+        ];
