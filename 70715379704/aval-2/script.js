66eb358aaf86eaa2e0c4411c625e2bd27711286c
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-10-24 08:23:20 -0300

    publish(70715379704): 2

---
+++ 70715379704/aval-2/script.js
@@ -0,0 +1,12 @@
+<script>
+    // Mensagem de boas-vindas animada
+    window.addEventListener("load", function() {
+        let mensagem = document.createElement("div");
+        mensagem.textContent = "🚛 Bem-vindo ao site O Mundo dos Caminhoneiros!";
+        mensagem.style.position = "fixed";
+        mensagem.style.top = "10px";
+        mensagem.style.left = "50%";
+        mensagem.style.transform = "translateX(-50%)";
+        mensagem.style.background = "#006600";
+        mensagem.style.color = "white";
+        mensagem.style.padding = "10px
