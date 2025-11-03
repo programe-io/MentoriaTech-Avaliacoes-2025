cf9caffe89469d95eb456289a2be6fe3aecc6b55
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-11-03 12:54:57 -0300

    publish(71492364843): 2

---
+++ 71492364843/aval-2/script.js
@@ -0,0 +1,7 @@
+// Seleciona o botão pelo ID
+const botao = document.getElementById('btnMensagem');
+
+// Adiciona evento de clique
+botao.addEventListener('click', function() {
+    alert('Bem-vindo à Academia Kleuton! Vamos treinar juntos!');
+});
