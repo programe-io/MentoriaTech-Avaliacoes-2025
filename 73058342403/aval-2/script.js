17240d0e20e0c055711fc20f9840c4db033afec3
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-11-04 10:10:06 -0300

    publish(73058342403): 2

---
+++ 73058342403/aval-2/script.js
@@ -0,0 +1,3 @@
+const footer = document.querySelector("footer p");
+const ano = new Date().getFullYear();
+footer.innerHTML = `&copy; ${ano} - Todos os Direitos Reservados - Mentoria Tech | Blog pessoal desenvolvido em ${ano}`;
