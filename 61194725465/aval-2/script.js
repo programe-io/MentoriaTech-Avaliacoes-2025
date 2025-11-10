aa6ce905d1c865cc84649aee12c5fe05711a2e56
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-11-10 17:27:30 -0300

    publish(61194725465): 2

---
+++ 61194725465/aval-2/script.js
@@ -0,0 +1,5 @@
+// Exibe uma mensagem no site
+alert("Olá! Seja bem-vindo ao meu código JavaScript!");
+
+// Mostra uma mensagem no console do navegador
+console.log("O JavaScript está funcionando perfeitamente!");
