e58aa4fe67dcf1ad88f34764a4ceda6fb3c3931e
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-11-11 08:06:24 -0300

    publish(65113977551): 3

--- 65113977551/aval-3/script.js
+++ 65113977551/aval-3/script.js
@@ -1,24 +1,7 @@
-document.getElementById("btnYoutube").addEventListener("click", function() {
-      window.open("https://m.youtube.com/watch?v=XX3dmVrSRA4&pp=ygUMYXlydG9uIHNlbm5h", "_blank");
-      });
-
-      function atualizarRelogio() {
-        const agora = new Date();
-          document.getElementById("relogio").innerText =
-              "⏰ Horário atual: " + agora.toLocaleTimeString();
-              }
-              setInterval(atualizarRelogio, 1000);
-              atualizarRelogio();
-
-              const frases = [
-                "Se você quer ser bem-sucedido, precisa ter dedicação total.",
-                  "O medo faz parte da vida da gente. Algumas pessoas não sabem como enfrentá-lo.",
-                    "Vencer é o que importa. O resto é a consequência."
-                    ];
-
-                    function mostrarFrase() {
-                      const fraseAleatoria = frases[Math.floor(Math.random() * frases.length)];
-                        document.getElementById("frase").innerText = "💬 " + fraseAleatoria;
-                        }
-
-                        mostrarFrase();
+function showTime() {
+	document.getElementById('currentTime').innerHTML = new Date().toUTCString();
+}
+showTime();
+setInterval(function () {
+	showTime();
+}, 1000);
