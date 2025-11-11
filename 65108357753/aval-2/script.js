e26cf9c4c8f146bf924940581002655905c69876
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-11-11 16:10:15 -0300

    publish(65108357753): 2

---
+++ 65108357753/aval-2/script.js
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>Blog da Mariana</title>
+
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <div class="container">
+    <header class="banner">
+      <h1>Mariana</h1>
+    </header>
+
+    <main>
+      <section class="card">
+        <h2>Sobre mim</h2>
+        <p>Minha comida preferida é macarrão, minha cor preferida é roxo e o que eu gosto de fazer é dormir.</p>
+      </section>
+
+      <section class .="card">
+        <h2>Comida Preferida</h2>
+        <p>Adoro macarrão porque é gostoso e combina com vários tipos de molho.</p>
+      </section>
+
+      <section class="card">
+        <h2>O que eu gosto de fazer</h2>
+        <p>Eu gosto de dormir, porque me relaxa e me deixa feliz.</p>
+      </section>
+    </main>
+
+    <button id="tema-btn">Trocar Tema</button>
+
+    <footer>
+      <p>Feito por Mariana • <span id="ano"></span></p>
+    </footer>
+  </div>
+
+  <script src="script.js"></script>
+</body>
+</html>
