dcb57232dc1106430a1394084d5cf79382302e87
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-11-14 13:41:58 -0300

    publish(71600411983): 3

---
+++ 71600411983/aval-3/script.js
@@ -0,0 +1,10 @@
+// Exibe uma mensagem no console
+console.log("Olá! Este é um JavaScript simples.");
+
+// Mostra um alerta na tela
+alert("Seja bem-vindo ao meu site!");
+
+// Muda o texto de um elemento HTML
+function mudarTexto() {
+    document.getElementById("mensagem").innerText = "O texto foi alterado pelo JavaScript!";
+}
