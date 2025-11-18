ecfccd569fc0f6032acbf635aa755fee69b23ba1
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-11-18 10:05:06 -0300

    publish(74369247578): 2

---
+++ 74369247578/aval-2/script.js
@@ -0,0 +1,6 @@
+// Script simples para demonstrar uso de JavaScript
+console.log("Site carregado com sucesso!");
+
+document.querySelector(".menu").addEventListener("click", () => {
+    console.log("Menu clicado!");
+});
