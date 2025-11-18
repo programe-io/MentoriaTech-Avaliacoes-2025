743e02c33973973d01303a021a8f3c3b03c18f6b
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-11-18 08:58:47 -0300

    publish(73618253400): 3

---
+++ 73618253400/aval-3/script.js
@@ -0,0 +1,4 @@
+document.getElementById("botaoInfo").addEventListener("click", function() {
+    document.getElementById("curiosidade").textContent = 
+        "Curiosidade: o futsal surgiu no Uruguai em 1934!";
+});
