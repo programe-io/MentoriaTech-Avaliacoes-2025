5f54004b27efd856775c9d5e9be3cf840bfea295
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-11-18 08:37:16 -0300

    publish(36494722091): 3

---
+++ 36494722091/aval-3/script.js
@@ -0,0 +1,6 @@
+/* JavaScript: Adiciona a interatividade */
+const meuBotao = document.getElementById("meuBotao");
+
+meuBotao.addEventListener("click", function() {
+  alert("Você clicou no botão!");
+});
