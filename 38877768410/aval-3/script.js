038dbc24a957e2cb39982cf86c3cf65b4fbef83f
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-11-21 10:09:00 -0300

    publish(38877768410): 3

--- 38877768410/aval-3/script.js
+++ 38877768410/aval-3/script.js
@@ -1,9 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Ao clicar em imagens da galeria, abrir em nova aba
-  document.querySelectorAll(".galeria img").forEach(function(img){
-    img.addEventListener("click", function(){
-      // se a src for válida, abre em nova aba
-      if (img.src) window.open(img.src, "_blank");
-    });
-  });
-});
+console.log("Blog do Show da Luna carregado corretamente.");
