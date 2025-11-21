9f526aa397bc969b32f6fc2c754d7a7ead9e7c19
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-11-21 10:17:39 -0300

    publish(70602078920): 3

---
+++ 70602078920/aval-3/script.js
@@ -0,0 +1,23 @@
+const brinquedos = [
+    {
+        id: 1,
+        vendedor: "@LojaFeliz",
+        avatar: "https://www.lojafeliz.com/images/logo.png", // Substitua por um link de avatar real
+        imagem: "brinquedo1.jpg", // Substitua por link de imagem real
+        preco: "R$ 49,90",
+        legenda: "Quebra-cabeça 3D do Dragão! Perfeito para desenvolver o raciocínio. #QuebraCabeça #Brinquedos",
+        likes: 150,
+        comentarios: 12
+    },
+    {
+        id: 2,
+        vendedor: "@MundoKids",
+        avatar: "avatar2.jpg",
+        imagem: "brinquedo2.jpg",
+        preco: "R$ 129,99",
+        legenda: "Super Pista de Corrida! Diversão garantida para a criançada. Deslize para ver o vídeo! #Carrinhos #PistaDeCorrida",
+        likes: 320,
+        comentarios: 45
+    },
+    // Adicione mais brinquedos aqui
+];
