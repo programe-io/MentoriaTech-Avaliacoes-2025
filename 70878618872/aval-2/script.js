09b200c47eea451900710e711f4467d78173ed34
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-11-24 20:16:49 -0300

    publish(70878618872): 2

---
+++ 70878618872/aval-2/script.js
@@ -0,0 +1,5 @@
+function mostrarCuriosidades() {
+        let bloco = document.getElementById("curiosidades");
+            bloco.style.display = bloco.style.display === "none" ? "block" : "none";
+            }
+}
