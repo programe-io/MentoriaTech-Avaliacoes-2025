25455d28426143ae49ace745ccbb66ae1e790316
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-11-25 15:17:38 -0300

    publish(72236784732): 2

---
+++ 72236784732/aval-2/script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const btn = document.querySelector(".btn");
+
+    btn.addEventListener("click", () => {
+        alert("Você será redirecionado para a página oficial de download do Free Fire!");
+    });
+
+    console.log("Página carregada com sucesso.");
+});
