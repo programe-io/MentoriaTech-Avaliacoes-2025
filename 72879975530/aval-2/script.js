66e1c0d6b5e21b950f6eca880dffa1eacb46df9c
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-11-25 08:47:30 -0300

    publish(72879975530): 2

--- 72879975530/aval-2/script.js
+++ 72879975530/aval-2/script.js
@@ -1,3 +1,7 @@
 function mostrarMensagem() {
     document.getElementById("mensagem").textContent = "Você clicou no botão! 🎉";
 }
+function mudarTexto() {
+    const p = document.getElementById("texto");
+    p.textContent = "Você clicou no botão! 🎉";
+}
