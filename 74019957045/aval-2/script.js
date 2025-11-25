a9e5c50c068ebba07bfb518277aa975032319cd8
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-11-25 15:37:56 -0300

    publish(74019957045): 2

---
+++ 74019957045/aval-2/script.js
@@ -0,0 +1,5 @@
+// Define automaticamente o ano atual no rodapé
+document.getElementById("ano").textContent = new Date().getFullYear();
+
+// Exemplo de ação simples
+console.log("Página carregada: Henrique & Juliano");
