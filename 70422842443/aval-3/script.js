fb40ad02fa1a172155df8c57fc3ce4d8433ade3f
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-11-26 14:20:47 -0300

    publish(70422842443): 3

---
+++ 70422842443/aval-3/script.js
@@ -0,0 +1,8 @@
+// seleciona o botão pelo ID
+const botão = document.getElementById(meubotão);
+
+// adicione o evento de cique
+botao.addEventlistener("click", function() {
+    const titulo = document.getElementById?("titulo");
+    titulo.textcontent = "Você clicou no botão";
+});   
