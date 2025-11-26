75e5dbe32606ee4bbc680bcdcd3fcaec31010d49
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-11-26 11:12:30 -0300

    publish(71557341567): 3

---
+++ 71557341567/aval-3/script.js
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gloss Lovers</title>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script> 
+    </head>
+
+<body class="bg-gradient-to-b from-pink-50 to-pink-200 h-full min-h-screen text-gray-800">
+    
+    <header class="shadow-lg p-5 flex bg-white">
+
+        <div class="flex-1 uppercase font-bold text-center md:text-left text-rose-600 text-xl tracking-widest">
+            💋 Gloss
+        </div>
+
+        <nav class="font-semibold gap-6 hidden md:flex">
+            <a href="#" class="hover:text-rose-600 transition duration-300">Início</a>
+            <a href="https://imperiumbrazil.com.br/?gad_source=1&gad_campaignid=22741023080&gbraid=0AAAAA-RC9vi2p1emQgMKIp_AtDzcUmDQg&gclid=Cj0KCQiAxJXJBhD_ARIsAH_JGji_v7aTOTJZI77SXsqU1iWKCjuZUipBjqmF4FbNAUjIUuRsqBS_ZkMaAvavEALw_wcB" class="hover:text-rose-600 transition duration-300">Makes</a>
+            <a href="https://www.lojafenzzamakeup.com.br/gloss" class="hover:text-rose-600 transition duration-300">Gloss</a>
+        </nav>
+
+    </header>
+
+      <main class="py-10 px-4 flex flex-col gap-6 md:flex-row md:justify-center md:items-start">
+ 
+        <div class="bg-white py-6 px-4 rounded-xl shadow-2xl max-w-sm w-full md:w-1/3">
+
+            <div class="flex items-center pb-4 gap-3">
+                <img class="h-12 w-12 rounded-full border-2 border-rose-500 object-cover" src="https://m.media-amazon.com/images/I/714F33ssHhL._AC_SX679_.jpg" alt="Gloss Kiko">
+                <h2 class="text-xl font-bold text-rose-600">Kiko Milano</h2>
+            </div>
+            
+          <img class="w-full rounded-t-lg object-cover" src="https://m.media-amazon.com/images/I/714F33ssHhL._AC_SX679_.jpg" alt="Foto do Gloss Kiko">
+
+          <div class="pt-4">
+             <p class="text-sm text-justify leading-relaxed text-gray-700 mb-4">
+                 O gloss Kiko é uma verdadeira joia para os lábios: com textura cremosa e um brilho intenso, ele entrega aquele **efeito molhado elegante** sem pesar. Sua fórmula é hidratante, cuidando dos lábios enquanto realça o tom natural com um toque luminoso.
+             </p>
+
+             <span class="text-xs text-gray-400 text-right block">
+                 há 10 minutos
+             </span>
+          </div>
+        </div>
+
+        <div class="bg-white py-6 px-4 rounded-xl shadow-2xl max-w-sm w-full md:w-1/3">
+
+            <div class="flex items-center pb-4 gap-3">
+                <img class="h-12 w-12 rounded-full border-2 border-rose-500 object-cover" src="https://m.media-amazon.com/images/I/51ijZ8zryKL._AC_SY300_SX300_QL70_ML2_.jpg" alt="Gloss LipHoney">
+                <h2 class="text-xl font-bold text-rose-600">LipHoney</h2>
+            </div>
+
+          <img class="w-full rounded-t-lg object-cover" src="https://m.media-amazon.com/images/I/51ijZ8zryKL._AC_SY300_SX300_QL70_ML2_.jpg" alt="Foto do Gloss LipHoney">
+
+          <div class="pt-4">
+            <p class="text-sm text-justify leading-relaxed text-gray-700 mb-4">
+                O Gloss LipHoney da Fran by Franciny Ehlke é a combinação perfeita entre beleza e tratamento para os lábios. Com uma textura leve e confortável, inspirada no mel, ele proporciona um **brilho intenso e elegante**, sem ser pegajoso. Sua fórmula é enriquecida com Extrato de Mel e Vitamina E, garantindo hidratação prolongada.
+            </p>
+
+            <span class="text-xs text-gray-400 text-right block">
+                há 25 minutos
+            </span>
+
+          </div>
+        </div>
+
+        <div class="bg-white py-6 px-4 rounded-xl shadow-2xl max-w-sm w-full md:w-1/3">
+
+            <div class="flex items-center pb-4 gap-3">
+                <img class="h-12 w-12 rounded-full border-2 border-rose-500 object-cover" src="https://m.media-amazon.com/images/I/61MxcmwoqHL._AC_SY300_SX300_QL70_ML2_.jpg" alt="Gloss Carmed">
+                <h2 class="text-xl font-bold text-rose-600">Carmed</h2>
+            </div>
+            
+          <img class="w-full rounded-t-lg object-cover" src="https://m.media-amazon.com/images/I/61MxcmwoqHL._AC_SY300_SX300_QL70_ML2_.jpg" alt="Foto do Gloss Carmed">
+          
+          <div class="pt-4">
+            <p class="text-sm text-justify leading-relaxed text-gray-700 mb-4">
+                 O Gloss Labial Carmed, conhecido por sua alta qualidade e ótimo custo-benefício, é a escolha perfeita para quem deseja lábios bonitos e bem cuidados. Este gloss se destaca por oferecer um **brilho espelhado incrível** sem a sensação pegajosa, proporcionando conforto imediato ao aplicar.
+            </p>
+
+            <span class="text-xs text-gray-400 text-right block">
+                há 7 minutos
+            </span>
+
+          </div>
+        </div>
+
+    </main>
+
+</body>
+</html>
