0a8f16007a207f732654a4fd29fa11eb96a97992
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-11-27 19:30:03 -0300

    publish(64649845726): 3

---
+++ 64649845726/aval-3/script.js
@@ -0,0 +1,7 @@
+const cards = document.querySelectorAll(".card");
+
+cards.forEach(card => {
+    card.addEventListener("click", () => {
+            alert("Conteúdo em breve! 🐾");
+                });
+                });
