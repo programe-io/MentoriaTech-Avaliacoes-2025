a46beee1f3e2a41510b157a66a766702313f2f4e
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-11-27 10:10:43 -0300

    publish(66485611436): 2

---
+++ 66485611436/aval-2/script.js
@@ -0,0 +1,15 @@
+{
+    scroll - behavior;
+}
+
+const sections = document.querySelectorAll("section, aside");
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("visible");
+        }
+    });
+}, { threshold: 0.2 });
+
+sections.forEach(sec => observer.observe(sec));
