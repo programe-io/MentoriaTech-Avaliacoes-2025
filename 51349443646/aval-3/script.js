f702197e8b31c533e07af6014e945c2f3f27cf2c
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-11-27 14:18:42 -0300

    publish(51349443646): 3

---
+++ 51349443646/aval-3/script.js
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Social Feed</title>
+  <link href="./output.css" rel="stylesheet">
+</head>
+
+<body class="bg-gradinte-to-b  from-blue-900  to-blue-950 h-acreen  text-white">
+
+    <header class="shadow-md p-5 flex">
+        
+        <div class="flex-1 uppercase fonte-bold text-center md:text-left">
+            logo
+        </div>    
+        
+
+        <nav class="fonte-bold flex gap-3">
+            <a href="">Inicio</a>
+            <a href="">Amaigos</a>
+            <a href="">Petfil</a>
+        </nav>
+    
+    </header>
+
+    <!-- POST LISTS -->
+    <main class="py-4 px-2 flex-col gap-3 md:flex-row">
+
+        <!-- POST CARD -->
+        <div class="bg-gray-800 py-4 rounded-lg shadow-md max-w-96">
+        
+                  
+            <!-- CARD HEADER -->
+            <div class="flex items-center pb-4 px-4 gap-3">
+           
+                <img class="h-12 w-12 rounded-full border-2 border-blue-500" src="imades/profile_01.jpg" alt="">
+                <h2 class="text-2x1">FroJho</h2>
+
+            </div>
+        
+            <!-- CARD IMAGE -->
+            <img src="w-full" src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fbr.pinterest.com%2Fwallpaperdequalidade%2Fwallpaper%2F&psig=AOvVaw0ES6sRAmEwIPwn1e-ZsYUG&ust=1764340031982000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCJD4qfrEkpEDFQAAAAAdAAAAABAE" alt="">
+            
+            <!-- CARD CONTENT -->
+        
+            </div class="pt-4 px-4">
+
+                 <p class="text-sm text-justify">
+                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod beatae
+                     quas nihil sed fugit voluptatibus repellat delectus ut tempora debitis
+                     est dolorum deserunt.
+                 </p>
+            
+                 <span class="text-xs text-gray-300 text-right block mt-4">
+                     ha 10 minitos
+                 </span>
