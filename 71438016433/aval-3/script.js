479d3c25d2e4dab870c4ddd823dc3a103cd37eed
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-11-28 13:07:42 -0300

    publish(71438016433): 3

---
+++ 71438016433/aval-3/script.js
@@ -0,0 +1,12 @@
+<p class="text-sm text-justify">
+    </p>
+
+<div class="flex items-center justify-between pt-3 pb-2">
+    <button class="like-button flex items-center gap-1 text-gray-400 hover:text-red-500 transition-colors" data-likes="0">
+        <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clip-rule="evenodd"></path></svg>
+        <span class="like-count">0</span> Curtidas
+    </button>
+</div>
+<span class="text-xs text-gray-300 text-right block md-4">
+    há X minutos
+</span>
