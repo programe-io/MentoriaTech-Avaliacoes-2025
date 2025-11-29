e476d734bcba36a8733156aeac66370d94192a24
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-11-29 19:21:18 -0300

    publish(25821812160): 3

--- 25821812160/aval-3/script.js
+++ 25821812160/aval-3/script.js
@@ -1,20 +1,5 @@
-// Função para gerar uma cor hexadecimal aleatória
-function gerarCorAleatoria() {
-    const cor = '#' + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0');
-        return cor;
-        }
-
-        // Espera o documento carregar
-        document.addEventListener('DOMContentLoaded', function() {
-            // Seleciona o elemento h1 pelo ID
-                const nomeElemento = document.getElementById('meuNome');
-
-                    // Adiciona o evento de clique
-                        nomeElemento.addEventListener('click', function() {
-                                // Pega a nova cor aleatória
-                                        const novaCor = gerarCorAleatoria();
-                                                
-                                                        // Aplica a nova cor ao estilo do elemento
-                                                                nomeElemento.style.color = novaCor;
-                                                                    });
-                                                                    });
+// script.js
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Ebook Tecnologia Hoje carregado com sucesso!");
+  });
+  
