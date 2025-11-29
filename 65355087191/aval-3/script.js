fc8c1d78bcefd27ea74e25420e2db10c74a4985d
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-11-29 20:11:35 -0300

    publish(65355087191): 3

---
+++ 65355087191/aval-3/script.js
@@ -0,0 +1,5 @@
+document.getElementById("botaoMensagem").addEventListener("click", function(){
+      document.getElementById("mensagem").textContent = "Você clicou! Obrigado.";
+      });
+      console.log("Script carregado com sucesso.");
+})
