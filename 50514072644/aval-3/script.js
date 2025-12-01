de99e357a3ca55fcfbc54a7a9a5ea02e1a37e6de
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-12-01 14:41:30 -0300

    publish(50514072644): 3

--- 50514072644/aval-3/script.js
+++ 50514072644/aval-3/script.js
@@ -1,1 +1,4 @@
-console.log("Blog da Peppa Pig — por Laryne Guedes — carregado com sucesso!");
+// script.js — coloque na mesma pasta que index.html
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Blog Peppa Pig carregado — criado por Laryne Guedes");
+});
