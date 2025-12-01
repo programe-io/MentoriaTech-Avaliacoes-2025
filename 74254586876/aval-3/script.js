5410a27b800cc46bc84af26eee961aee10639b5a
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-12-01 14:23:04 -0300

    publish(74254586876): 3

---
+++ 74254586876/aval-3/script.js
@@ -0,0 +1,4 @@
+document.getElementById("frevoBtn").addEventListener("click", function() {
+  const info = document.getElementById("frevoInfo");
+  info.textContent = "🎶 O frevo é um ritmo musical e uma dança típica de Pernambuco, especialmente no carnaval!";
+});
