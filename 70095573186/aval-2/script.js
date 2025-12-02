5c06e2bd3869782b5ad02834c8b6b99021d1a98a
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-12-02 15:14:43 -0300

    publish(70095573186): 2

---
+++ 70095573186/aval-2/script.js
@@ -0,0 +1,9 @@
+function lerMais(id) {
+    const textos = {
+        1: "Breath of the Wild revolucionou a franquia Zelda, trazendo um mundo aberto gigantesco, física avançada e liberdade total para explorar Hyrule.",
+        2: "God of War (2018) mostra Kratos em uma jornada emocional com seu filho Atreus, enfrentando deuses e criaturas da mitologia nórdica.",
+        3: "Super Mario Odyssey traz fases criativas, habilidades novas com Cappy e uma jogabilidade divertida para todas as idades."
+    };
+
+    alert(textos[id]);
+}
