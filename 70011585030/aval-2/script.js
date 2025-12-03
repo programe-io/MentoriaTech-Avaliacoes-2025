df7a1d0162b09c2308b91f6df58d1ec4a8492c94
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-12-03 09:55:08 -0300

    publish(70011585030): 2

---
+++ 70011585030/aval-2/script.js
@@ -0,0 +1,3 @@
+A tag obrigatória "h3" não foi encontrada no seu código HTML. Por favor, revise e tente novamente.
+
+Você deve seguir uma estrutura semelhante ao projeto que foi proposto no vídeo da avaliação.
