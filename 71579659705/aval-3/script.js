ab518ebfae2d67ad599e35e396c821117eb4cb40
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-12-04 13:56:40 -0300

    publish(71579659705): 3

--- 71579659705/aval-3/script.js
+++ 71579659705/aval-3/script.js
@@ -1,45 +1,42 @@
 const posts = [
     {
-        user: "FroJho",
-        avatar: "https://i.imgur.com/7k12F.png",
-        image: "https://kaleoz-media.seagmcdn.com/kaleoz-store/202407/oss-787364ef1b5e5051acc8788647a9c4ce.png",
-        time: "há 10 minutos"
+        nome: "Manomax",
+        perfil: "https://yt3.googleusercontent.com/8lqtY6SS5cOl6RapF1BKlVXdhQMf2MBzn2TZpIB3izdLyYRS3mixvrNQ8YmunbCJPWR95LvzFA=s900-c-k-c0x00ffffff-no-rj",
+        imagem: "https://kaleoz-media.seagmcdn.com/kaleoz-store/202407/oss-787364ef1b5e5051acc8788647a9c4ce.png",
+        texto: "Rei do 4x4",
+        tempo: "há 10 minutos"
     },
     {
-        user: "FroJho",
-        avatar: "https://i.imgur.com/7k12F.png",
-        image: "https://apkbomb.com/wp-content/uploads/2025/05/free-fire-max-1-3.png",
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod beatae quas nihil sed fugit voluptatibus repellat delectus ut tempora debitis est dolorum deserunt.",
-        time: "há 10 minutos"
+        nome: "LevelUp",
+        perfil: "https://yt3.googleusercontent.com/u9juATWi54PQsNo6CAwrokOsBARcs8TSU0m8Ng9BK9w7U9GdGy2WoS-caTzrwJMmEWWQCw975A=s900-c-k-c0x00ffffff-no-r",
+        imagem: "https://play-lh.googleusercontent.com/fPV15zPzpECONm08K6BUS5EqD1A1Ir_hxsOaaJF7hOIK-BNDpFO-i3MAvUVM7952JJyGAhg1VJwzDKtYT2QB8Ns=w240-h480-rw",
+        texto: "ganhou um mundial",
+        tempo: "há 10 minutos"
     },
     {
-        user: "FroJho",
-        avatar: "https://i.imgur.com/7k12F.png",
-        image: "https://play-lh.googleusercontent.com/fPV15zPzpECONm08K6BUS5EqD1A1Ir_hxsOaaJF7hOIK-BNDpFO-i3MAvUVM7952JJyGAhg1VJwzDKtYT2QB8Ns=w240-h480-rw",
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod beatae quas nihil sed fugit voluptatibus repellat delectus ut tempora debitis est dolorum deserunt.",
-        time: "há 10 minutos"
+        nome: "Apelapato",
+        perfil: "https://i.pinimg.com/736x/f2/79/ef/f279efd36e6fe8e74ecf8b50e181dc88.jpg",
+        imagem: "https://apkbomb.com/wp-content/uploads/2025/05/free-fire-max-1-3.png",
+        texto: "criador da agachadinha",
+        tempo: "há 10 minutos"
     }
 ];
 
-const container = document.querySelector(".feed-container");
+const feed = document.getElementById("feed");
 
 posts.forEach(post => {
-    container.innerHTML += `
-        <div class="card">
-            <div class="card-header">
-                <img src="${post.avatar}" alt="avatar">
-                <h3>${post.user}</h3>
-            </div>
-
-            <img src="${post.image}" class="post-image">
-
-            <div class="card-content">
-                ${post.text}
-            </div>
-
-            <div class="card-footer">
-                ${post.time}
-            </div>
+    feed.innerHTML += `
+    <div class="post">
+        <div class="post-header">
+            <img src="${post.perfil}" alt="">
+            <span class="post-title">${post.nome}</span>
         </div>
+
+        <img class="post-img" src="${post.imagem}" alt="Post Image">
+
+        <p class="post-text">${post.texto}</p>
+
+        <p class="post-time">${post.tempo}</p>
+    </div>
     `;
 });
