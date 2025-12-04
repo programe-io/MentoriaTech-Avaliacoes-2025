d6f7f9427bbd4b9512356a6a842199f1d26aa2ea
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-12-04 09:24:59 -0300

    publish(71001374312): 2

---
+++ 71001374312/aval-2/script.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Exemplo JS Básico</title>
+</head>
+<body>
+    <h1>Minha Primeira Página com JavaScript</h1>
+    <button onclick="mostrarMensagem()">Clique aqui</button>
+
+    <script>
+        function mostrarMensagem() {
+            alert("Olá! Você clicou no botão!");
+        }
+    </script>
+</body>
+</html>
