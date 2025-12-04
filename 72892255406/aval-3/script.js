4f35b49b9a6991eb542442ad9d8be4c2ee1b23bb
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-12-04 11:57:14 -0300

    publish(72892255406): 3

---
+++ 72892255406/aval-3/script.js
@@ -0,0 +1,15 @@
+// Lista de posts
+const posts = [
+    {
+        user: "FroJho",
+        avatar: "https://i.pravatar.cc/50",
+        image: "https://picsum.photos/id/29/600/300",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod beatae quas nihil sed fugit.",
+        time: "há 10 minutos"
+    },
+    {
+        user: "FroJho",
+        avatar: "https://i.pravatar.cc/51",
+        image: "https://picsum.photos/id/1018/600/300",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus repellat delectus tempora.",
+        time: "há 10 minuto
