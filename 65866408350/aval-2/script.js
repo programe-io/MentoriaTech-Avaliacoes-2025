20435a872ab91f4a8e9541924c228d6d24f35bec
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-12-04 08:27:39 -0300

    publish(65866408350): 2

---
+++ 65866408350/aval-2/script.js
@@ -0,0 +1,7 @@
+console.log("Site Harry Potter carregado!");
+
+// Pequena interação: clique no título
+document.querySelector("h1").addEventListener("click", () => {
+  alert("✨ Bem-vindo a Hogwarts!");
+});
+
