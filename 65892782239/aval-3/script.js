2eee692c98d6c67e9525678549f32201711100f4
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-12-04 09:26:35 -0300

    publish(65892782239): 3

---
+++ 65892782239/aval-3/script.js
@@ -0,0 +1,9 @@
+// Alternar tema claro/escuro
+document.getElementById("temaBtn").addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+});
+
+// Efeito divertido ao clicar no título
+document.querySelector("h1").addEventListener("click", () => {
+    alert("A Escola Joel Ribeiro é MUITO animada! 🎉📚");
+});
