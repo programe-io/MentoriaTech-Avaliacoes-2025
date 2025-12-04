801cddfe1068243a9b9c9f72499416a995d96947
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-12-04 08:43:12 -0300

    publish(68872629454): 3

---
+++ 68872629454/aval-3/script.js
@@ -0,0 +1,7 @@
+function mensagemBoasVindas() {
+    alert("Bem-vindo ao CETI Joel Ribeiro!");
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Site do CETI Joel Ribeiro carregado com sucesso.");
+});
