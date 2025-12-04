8a7507955f90028b0b105cc9a3032ca30d85e791
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-12-04 08:14:23 -0300

    publish(68872629454): 2

---
+++ 68872629454/aval-2/script.js
@@ -0,0 +1,7 @@
+function mostrarMensagem() {
+    alert("Bem-vindo ao nosso portal educacional!");
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Site carregado com sucesso!");
+});
