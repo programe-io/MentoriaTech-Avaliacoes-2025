99ec058ed4f58fda3185ceb5024103832dd23d79
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-12-04 14:17:20 -0300

    publish(73520797380): 2

---
+++ 73520797380/aval-2/script.js
@@ -0,0 +1,4 @@
+gh repo clone jalinbypal/primeira-avalia-o-gleysondocument.getElementById("btnMensagem").addEventListener("click", function() {
+        alert("A turma de Desenvolvimento de Sistemas agradece sua visita ao nosso blog! 💻✨");
+        });
+})
