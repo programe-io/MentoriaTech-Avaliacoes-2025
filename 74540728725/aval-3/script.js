54b0e68daa2c4859de80c7dbd3063aa32652deb5
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-12-11 13:13:32 -0300

    publish(74540728725): 3

---
+++ 74540728725/aval-3/script.js
@@ -0,0 +1,9 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Main {
+    public static void main(String[] args) {
+            int ta = 19;
+                    System.out.println("bairro parque Piauí");
+                            public static void ("maria clara");
+                                    System.out.println(ta);
