8a01ccc36732d415e4b44e1208212826fa70c87d
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-12-11 08:05:41 -0300

    publish(71623566843): 3

---
+++ 71623566843/aval-3/script.js
@@ -0,0 +1,5 @@
+// Script básico para futuras interações
+document.querySelector('.contact-form').addEventListener('submit', function(e) {
+  e.preventDefault();
+  alert('Mensagem enviada com sucesso! Entraremos em contato em breve.');
+});
