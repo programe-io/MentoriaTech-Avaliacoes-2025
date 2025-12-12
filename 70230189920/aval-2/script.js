4223738a133b3a9b584d6433a3e44866013fc154
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-12-12 10:11:41 -0300

    publish(70230189920): 2

---
+++ 70230189920/aval-2/script.js
@@ -0,0 +1,17 @@
+.quiz-page-ask-me button.quiz-page-ask-me_btn {
+  width: 50px;
+  height: 40px;
+  min-width: 50px;
+  min-height: 40px;
+  padding: 8px;
+  border-radius: 0 8px 8px 0;
+  background: rgb(240, 94, 157);
+  background: linear-gradient(
+    90deg,
+    rgba(240, 94, 157, 0.7) 33%,
+    rgba(85, 245, 193, 0.7792366946778712) 65%
+  );
+  background-size: 200% 200%;
+  background-position: 50% 100%;
+  
+}
