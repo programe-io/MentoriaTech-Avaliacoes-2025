331273653e37ee54552034ed6c4c388cadb9d1f9
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2025-12-12 13:15:41 -0300

    publish(71518919820): 3

---
+++ 71518919820/aval-3/script.js
@@ -0,0 +1,33 @@
+// Curtir 👍🏽
+document.querySelector(".like-btn").addEventListener("click", function () {
+  this.classList.toggle("active");
+  });
+
+  // Comentar ✍️🏽
+  document.querySelector(".comment-btn").addEventListener("click", function () {
+    alert("Função de comentário ✍️🏽");
+    });
+
+    // UPLOAD
+    document.getElementById("uploadInput").addEventListener("change", function (event) {
+      const file = event.target.files[0];
+        if (!file) return;
+
+          const reader = new FileReader();
+            reader.onload = function (e) {
+                const post = document.createElement("div");
+                    post.className = "post";
+
+                        post.innerHTML = `
+                              <img class="post-img" src="${e.target.result}">
+                                    <div class="post-actions">
+                                            <span class="like-btn">👍🏽</span>
+                                                    <span class="comment-btn">✍️🏽</span>
+                                                          </div>
+                                                              `;
+
+                                                                  document.querySelector("main").appendChild(post);
+                                                                    };
+
+                                                                      reader.readAsDataURL(file);
+                                                                      });
